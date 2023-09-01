b444c91e37b5b1901a4727014daff067e4184483
Mahalakshmiloganathan <129155434+Mahalakshmiloganathan@users.noreply.github.com>
2023-09-01 17:23:43 +0530

    CalculatorApp Assignments

--- assignments/corejava/core-java-assignments/src/calculatorapp/CalculatorApp.java
+++ assignments/corejava/core-java-assignments/src/calculatorapp/CalculatorApp.java
@@ -1,52 +1,27 @@
 package calculatorapp;
 
-import java.util.Scanner;
 
 public class CalculatorApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Calculator App");
-        System.out.println("Select one operation");
-        System.out.println(" 1. Addition \n 2. Subtraction \n 3. Multiply \n 4. Divided \n 5. Exit" );
-        int output = sc.nextInt();
-        System.out.print("Enter the first number: ");
-        double a = sc.nextDouble();
-        System.out.print("Enter the second number: ");
-        double b = sc.nextDouble();
-        switch (output)
-        {
-            case 1: {
-                double sum = a + b;
-                System.out.println(" Addition of a + b = " + sum);
-                break;
-            }
-            case 2: {
-                double prod = a - b;
-                System.out.println(" Subtraction of a - b = " + prod);
-                break;
-            }
-            case 3: {
-                double mul = a * b;
-                System.out.println(" Multiplication of a - b = " + mul);
-                break;
-            }
-            case 4: {
-                if (b!=0) {
-                    double div = a / b;
-                    System.out.println(" Division of a - b = " + div);
-                }else{
-                    System.out.println(" Cannot divide by zero ");
-                }
 
-                break;
-            }
-            case 5: {
-                System.out.println(" Exiting the program ");
-                break;
-            }
-            default:
-                System.out.println(" Invalid operation number ");
-                break;
-        }
+        int value1 = 10;
+        int value2 = 2;
+
+        //Adition
+        int add = value1+value2;
+        System.out.println("Addition of value1 + value2 = " + add);
+
+        //subtraction
+        int sub = value1-value2;
+        System.out.println("Subtraction of value1 - value2 = " + sub);
+
+        //Multiplication
+        int mul = value1 * value2;
+        System.out.println("Multiplication of value1 * value2 = " + mul);
+
+        //division
+        int div = value1 / value2;
+        System.out.println("Division of value1 / value2 = " + div);
+
     }
 }
