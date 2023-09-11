83af12145a6972e127fa2dfc62e93af582eb4178
Mahalakshmiloganathan <129155434+Mahalakshmiloganathan@users.noreply.github.com>
2023-09-11 11:39:47 +0530

    BankingApp Using Array Changes

--- assignments/corejava/bankingapp-with-arrays/src/BankingAppMain.java
+++ assignments/corejava/bankingapp-with-arrays/src/BankingAppMain.java
@@ -2,7 +2,6 @@ import com.indium.bankingapp.model.Account;
 import com.indium.bankingapp.service.AccountService;
 import com.indium.bankingapp.service.AccountServiceArrImpl;
 
-
 import java.util.Scanner;
 
 public class BankingAppMain {
@@ -12,7 +11,6 @@ public class BankingAppMain {
         boolean exit = false;
 
         while (!exit) {
-
             System.out.println("\n Banking App Menu:");
             System.out.println("1. Add Account");
             System.out.println("2. View All Accounts");
@@ -27,107 +25,114 @@ public class BankingAppMain {
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter Account Number: ");
-                    int accountNumber = scanner.nextInt();
-                    scanner.nextLine();
-
-                    System.out.println("Enter Account Holder Name: ");
-                    String accountHolderName = scanner.nextLine();
-
-                    System.out.println("Enter Initial Balance: ");
-                    double balance = scanner.nextDouble();
-                    scanner.nextLine();
-
-                    System.out.println("Enter Account Type: ");
-                    String type = scanner.nextLine();
-
-                    Account newAccount = new Account(0, accountNumber, accountHolderName, balance, type);
-                    boolean created = accountService.createAccount(newAccount);
-
-                    if (created) {
-                        System.out.println("Account created successfully!");
-                    } else {
-                        System.out.println("Failed to create account. Maximum accounts reached Or Account number already exists");
-                    }
+                    createAccount(accountService, scanner);
                     break;
-
                 case 2:
-                    Account[] allAccounts = accountService.getAllAccounts();
-                    System.out.println("All Accounts:");
-
-                    for (int i = 0; i < allAccounts.length; i++) {
-                        Account currentAccount = allAccounts[i];
-                        if (currentAccount != null) {
-                            currentAccount.displayAccountInfo();
-                        }
-                    }
+                    viewAllAccounts(accountService);
                     break;
-
                 case 3:
-                    System.out.println("Enter Account ID : ");
-                    int viewAccountId = scanner.nextInt();
-                    Account viewAccount = accountService.getAccount(viewAccountId);
-                    if (viewAccount != null) {
-                        viewAccount.displayAccountInfo();
-                    } else {
-                        System.out.println("Account not found.");
-                    }
+                    viewAccount(accountService, scanner);
                     break;
-
                 case 4:
-                    System.out.println("Enter Account ID to update:");
-                    int updateAccountId = scanner.nextInt();
-                    Account updateAccount = accountService.getAccount(updateAccountId);
-                    if (updateAccount != null) {
-                        scanner.nextLine();
-                        System.out.println("Enter New Account Holder Name:");
-                        String updatedName = scanner.nextLine();
-                        System.out.println("Enter New Balance:");
-                        double updatedBalance = scanner.nextDouble();
-                        scanner.nextLine();
-                        System.out.println("Enter New Account Type:");
-                        String updatedType = scanner.nextLine();
-
-                        updateAccount.setAccountHolderName(updatedName);
-                        updateAccount.setBalance(updatedBalance);
-                        updateAccount.setType(updatedType);
-
-                        boolean updated = accountService.updateAccount(updateAccountId, updateAccount);
-                        if (updated) {
-                            System.out.println("Account updated successfully.");
-                        } else {
-                            System.out.println("Failed to update account.");
-                        }
-                    } else {
-                        System.out.println("Account not found.");
-                    }
+                    updateAccount(accountService, scanner);
                     break;
-
-
                 case 5:
-                    System.out.println("Enter Account ID to delete: ");
-                    int deleteAccountId = scanner.nextInt();
-                    Account deleteAccount = accountService.getAccount(deleteAccountId);
-
-                    if (deleteAccount != null) {
-                        boolean deleted = accountService.deleleAccount(deleteAccount);
-                        if (deleted) {
-                            System.out.println("Account deleted successfully!");
-                        } else {
-                            System.out.println("Failed to delete account.");
-                        }
-                    } else {
-                        System.out.println("Account not found.");
-                    }
+                    deleteAccount(accountService, scanner);
                     break;
-
                 case 6:
+                    exit = true;
                     System.out.println("Exiting the Banking App !");
-                    System.exit(0);
-
+                    break;
                 default:
                     System.out.println("Invalid choice. Please select a valid option.");
             }
+        }
+    }
+
+    private static void createAccount(AccountService accountService, Scanner scanner) {
+        System.out.println("Enter Account Number: ");
+        int accountNumber = scanner.nextInt();
+        System.out.println("Enter Account Holder Name: ");
+        String accountHolderName = scanner.next();
+        System.out.println("Enter Initial Balance: ");
+        double balance = scanner.nextDouble();
+        System.out.println("Enter Account Type: ");
+        String type = scanner.next();
+        Account newAccount = new Account(0, accountNumber, accountHolderName, balance, type);
+        boolean created = accountService.createAccount(newAccount);
+        if (created) {
+            System.out.println("Account created successfully!");
+        } else {
+            System.out.println("Failed to create account. Maximum accounts reached or account number already exists.");
+        }
+    }
+
+    private static void viewAllAccounts(AccountService accountService) {
+        Account[] allAccounts = accountService.getAllAccounts();
+        System.out.println("All Accounts:");
+
+        for (int i = 0; i < allAccounts.length; i++) {
+            Account currentAccount = allAccounts[i];
+            if (currentAccount != null) {
+                currentAccount.displayAccountInfo();
+            }
+        }
+    }
+
+    private static void viewAccount(AccountService accountService, Scanner scanner) {
+        System.out.println("Enter Account ID: ");
+        int viewAccountId = scanner.nextInt();
+        Account viewAccount = accountService.getAccount(viewAccountId);
+
+        if (viewAccount != null) {
+            viewAccount.displayAccountInfo();
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+
+    private static void updateAccount(AccountService accountService, Scanner scanner) {
+        System.out.println("Enter Account ID to update:");
+        int updateAccountId = scanner.nextInt();
+        Account updateAccount = accountService.getAccount(updateAccountId);
+
+        if (updateAccount != null) {
+            System.out.println("Enter New Account Holder Name:");
+            String updatedName = scanner.next();
+            System.out.println("Enter New Balance:");
+            double updatedBalance = scanner.nextDouble();
+            System.out.println("Enter New Account Type:");
+            String updatedType = scanner.next();
+
+            updateAccount.setAccountHolderName(updatedName);
+            updateAccount.setBalance(updatedBalance);
+            updateAccount.setType(updatedType);
+
+            boolean updated = accountService.updateAccount(updateAccountId, updateAccount);
+            if (updated) {
+                System.out.println("Account updated successfully.");
+            } else {
+                System.out.println("Failed to update account.");
+            }
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+
+    private static void deleteAccount(AccountService accountService, Scanner scanner) {
+        System.out.println("Enter Account ID to delete: ");
+        int deleteAccountId = scanner.nextInt();
+        Account deleteAccount = accountService.getAccount(deleteAccountId);
+
+        if (deleteAccount != null) {
+            boolean deleted = accountService.deleleAccount(deleteAccount);
+            if (deleted) {
+                System.out.println("Account deleted successfully!");
+            } else {
+                System.out.println("Failed to delete account.");
+            }
+        } else {
+            System.out.println("Account not found.");
         }
     }
 }
