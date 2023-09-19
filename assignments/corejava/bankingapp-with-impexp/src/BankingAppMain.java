e34e1fc0b839fa1fe112e281db23dadbd78e2a87
Mahalakshmiloganathan <129155434+Mahalakshmiloganathan@users.noreply.github.com>
2023-09-19 10:45:46 +0530

    BankingApp with imp/emp

---
+++ assignments/corejava/bankingapp-with-impexp/src/BankingAppMain.java
@@ -0,0 +1,208 @@
+import com.indium.bankingapp.model.Account;
+import com.indium.bankingapp.service.AccountService;
+import com.indium.bankingapp.service.AccountServiceImpl;
+
+import java.util.*;
+
+public class BankingAppMain<T extends Account> {
+    private AccountService<T> accountService;
+    private Scanner scanner;
+    private Map<Integer, T> accounts = new HashMap<>();
+
+    public BankingAppMain(AccountService<T> accountService) {
+        this.accountService = accountService;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() throws Exception {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("\nBanking App Menu:");
+            System.out.println("1. Add Account");
+            System.out.println("2. View All Accounts");
+            System.out.println("3. View Account");
+            System.out.println("4. Update Account");
+            System.out.println("5. Delete Account");
+            System.out.println("6. Print Statistics");
+            System.out.println("7. Import");
+            System.out.println("8. Export");
+            System.out.println("9. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    viewAllAccounts();
+                    break;
+                case 3:
+                    viewAccount();
+                    break;
+                case 4:
+                    updateAccount();
+                    break;
+                case 5:
+                    deleteAccount();
+                    break;
+                case 6:
+                    printStatistics();
+                    break;
+
+                case 7:
+                    importAccounts();
+                    break;
+
+                case 8:
+                    exportAccounts();
+                    break;
+
+                case 9:
+                    exit = true;
+                    System.out.println("Exiting the Banking App !");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
+    }
+
+    private void createAccount() throws Exception {
+        System.out.println("Enter Account Number: ");
+        String accountNumber = scanner.next();
+        System.out.println("Enter Account Holder Name: ");
+        String accountHolderName = scanner.next();
+        System.out.println("Enter Initial Balance: ");
+        double balance = scanner.nextDouble();
+        System.out.println("Enter Account Type: ");
+        String type = scanner.next();
+        T newAccount = (T) new Account(0, accountNumber, accountHolderName, balance, type);
+        boolean created = accountService.createAccount(newAccount);
+        if (created) {
+            System.out.println("Account created successfully!");
+        } else {
+            System.out.println("Failed to create account. Account number already exists.");
+        }
+    }
+
+    private void viewAccount() throws Exception {
+        System.out.print("Enter Account ID to get details: ");
+        int idToGet = scanner.nextInt();
+        T account = accountService.getAccount(idToGet);
+        if (account != null) {
+            account.displayAccountInfo();
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+
+    private void viewAllAccounts() throws Exception {
+        Collection<T> allAccounts = accountService.getAllAccounts();
+        if (!allAccounts.isEmpty()) {
+            System.out.println("All Accounts:");
+            for (T acc : allAccounts) {
+                acc.displayAccountInfo();
+            }
+        } else {
+            System.out.println("No accounts found.");
+        }
+    }
+
+    private void updateAccount() throws Exception {
+        System.out.println("Enter Account ID to update:");
+        int updateAccountId = scanner.nextInt();
+        T updateAccount = accountService.getAccount(updateAccountId);
+
+        if (updateAccount != null) {
+            System.out.println("Enter New Account Holder Name:");
+            String updatedName = scanner.next();
+            System.out.println("Enter New Balance:");
+            double updatedBalance = scanner.nextDouble();
+            System.out.println("Enter New Account Type:");
+            String updatedType = scanner.next();
+
+            updateAccount.setAccountHolderName(updatedName);
+            updateAccount.setBalance(updatedBalance);
+            updateAccount.setType(updatedType);
+
+            boolean updated = accountService.updateAccount(updateAccountId, updateAccount);
+            if (updated) {
+                System.out.println("Account updated successfully.");
+            } else {
+                System.out.println("Failed to update account.");
+            }
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+
+    private void deleteAccount() throws Exception {
+        System.out.println("Enter Account ID to delete: ");
+        int deleteAccountId = scanner.nextInt();
+        T deleteAccount = accountService.getAccount(deleteAccountId);
+
+        if (deleteAccount != null) {
+            boolean deleted = accountService.deleteAccount(deleteAccount);
+            if (deleted) {
+                System.out.println("Account deleted successfully!");
+            } else {
+                System.out.println("Failed to delete account.");
+            }
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+
+    private void printStatistics() throws Exception {
+        System.out.println("Printing Statistics:");
+
+        // a] No of accounts which has a balance more than 1 lac
+        long accountsWithBalanceMoreThanOneLac = accountService.countAccountsWithBalanceMoreThanOneLac();
+        System.out.println("a] No of accounts which have a balance more than 1 lac: " + accountsWithBalanceMoreThanOneLac);
+
+        // b] Show no of accounts by account type
+        Map<String, String> accountTypeCount = accountService.countAccountsByAccountType();
+        System.out.println("b] Show no of accounts by account type: " + accountTypeCount);
+
+        // c] Show no of accounts by account type with sorting
+        Map<String, Long> sortedAccountTypeCount = accountService.countAccountsByAccountTypeSorted();
+        System.out.println("c] Show no of accounts by account type with sorting: " + sortedAccountTypeCount);
+
+        // d] Show avg balance by account type
+        Map<String, Double> avgBalanceByAccountType = accountService.averageBalanceByAccountType();
+        System.out.println("d] Show avg balance by account type: " + avgBalanceByAccountType);
+
+        // e] List account ids whose account name contains given name
+        System.out.print("Enter the account name to search: ");
+        Scanner sc = new Scanner(System.in);
+        String searchName = sc.next();
+        List<String> accountIdsWithName = accountService.getAccountIdsByAccountNameContains(searchName);
+
+        if (accountIdsWithName.isEmpty()) {
+            System.out.println("No matching account names found.");
+        } else {
+            System.out.println("e] List account ids whose account name contains '" + searchName + "': " + accountIdsWithName);
+        }
+
+    }
+    private void importAccounts() {
+        accountService.importAccounts();
+        System.out.println("Accounts imported successfully!");
+    }
+
+    private void exportAccounts() {
+        accountService.exportAccounts();
+        System.out.println("Accounts exported successfully!");
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        AccountService<Account> accountService = new AccountServiceImpl<>();
+        BankingAppMain<Account> bankingApp = new BankingAppMain<>(accountService);
+        bankingApp.run();
+    }
+}
---
+++ assignments/corejava/bankingapp-with-impexp/src/com.indium.bankingapp/service/AccountServiceImpl.java
@@ -0,0 +1,212 @@
+package com.indium.bankingapp.service;
+
+import com.indium.bankingapp.model.Account;
+
+import java.io.*;
+import java.util.*;
+
+public class AccountServiceImpl<T extends Account> implements AccountService<T> {
+
+    private Map<Integer, T> accounts = new HashMap<>();
+    private int nextAccountId = 1;
+
+    @Override
+    public boolean createAccount(T account) throws Exception {
+        try {
+            if (account == null || accounts.containsValue(account)) {
+                return false;
+            }
+            account.setAccountId(nextAccountId++);
+            accounts.put(account.getAccountId(), account);
+            exportAccounts();
+            return true;
+        } catch (NullPointerException | IllegalStateException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean updateAccount(int id, T updatedAccount) throws Exception {
+        try {
+            if (!accounts.containsKey(id) || updatedAccount == null) {
+                return false;
+            }
+            updatedAccount.setAccountId(id);
+            accounts.put(id, updatedAccount);
+            return true;
+        } catch (NullPointerException | IllegalStateException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean deleteAccount(T account) throws Exception {
+        try {
+            if (account == null || !accounts.containsValue(account)) {
+                return false;
+            }
+            accounts.remove(account.getAccountId());
+            return true;
+        } catch (NullPointerException | UnsupportedOperationException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public T getAccount(int id) throws Exception {
+        try {
+            return accounts.get(id);
+        } catch (NullPointerException | IllegalArgumentException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Collection<T> getAllAccounts() throws Exception {
+        try {
+            return accounts.values();
+        } catch (NullPointerException | UnsupportedOperationException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+
+    @Override
+    // a] No of accounts which has balance more than 1 lac
+    public long countAccountsWithBalanceMoreThanOneLac() throws Exception {
+        long count = 0;
+        try {
+            for (T account : accounts.values()) {
+                if (account.getBalance() > 100000) {
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return count;
+    }
+
+    // b] Show no of account by account type
+    public Map<String, String> countAccountsByAccountType() throws Exception {
+        Map<String, String> accountTypeCount = new HashMap<>();
+        try {
+            for (T account : accounts.values()) {
+                String accountType = account.getAccountType();
+                String count = accountTypeCount.getOrDefault(accountType, "0");
+                accountTypeCount.put(accountType, String.valueOf(Integer.parseInt(count) + 1));
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return accountTypeCount;
+    }
+
+
+    // c] Show no of accounts by account type with sorting
+    public Map<String, Long> countAccountsByAccountTypeSorted() throws Exception {
+        Map<String, Long> accountTypeCount = new HashMap<>();
+        try {
+            for (T account : accounts.values()) {
+                String accountType = account.getAccountType();
+                accountTypeCount.put(accountType, accountTypeCount.getOrDefault(accountType, 0L) + 1);
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
+        return accountTypeCount;
+    }
+
+
+
+    // d] Show avg balance by account type
+    public Map<String, Double> averageBalanceByAccountType() throws Exception {
+        Map<String, Double> avgBalanceByType = new HashMap<>();
+        Map<String, Double> sumBalanceByType = new HashMap<>();
+        Map<String, Long> countByType = new HashMap<>();
+
+        try {
+            for (T account : accounts.values()) {
+                String accountType = account.getAccountType();
+                double balance = account.getBalance();
+
+                sumBalanceByType.put(accountType, sumBalanceByType.getOrDefault(accountType, 0.0) + balance);
+                countByType.put(accountType, countByType.getOrDefault(accountType, 0L) + 1);
+            }
+
+            for (String accountType : sumBalanceByType.keySet()) {
+                double sum = sumBalanceByType.get(accountType);
+                long count = countByType.get(accountType);
+                avgBalanceByType.put(accountType, sum / count);
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
+        return avgBalanceByType;
+    }
+
+    // e] List account ids whose account name contains given name
+    public List<String> getAccountIdsByAccountNameContains(String name) throws Exception {
+        List<String> accountIds = new ArrayList<>();
+        try {
+            for (T account : accounts.values()) {
+                if (account.getAccountHolderName().contains(name)) {
+                    accountIds.add(account.getAccountNumber());
+                }
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return accountIds;
+    }
+
+    @Override
+    public void importAccounts() {
+        try (BufferedReader br = new BufferedReader(new FileReader("./input/input.txt"))) {
+            String line;
+            int accountId = 1; // Initialize the account ID
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 4) {
+                    String accountNumber = parts[0];
+                    String accountHolderName = parts[1];
+                    double balance = Double.parseDouble(parts[2]);
+                    String type = parts[3];
+                    T account = (T) new Account(accountId, accountNumber, accountHolderName, balance, type);
+                    accounts.put(accountId, account); // Add the account to the map
+                    accountId++; // Increment the account ID for the next account
+                }
+            }
+            System.out.println("Accounts imported successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error while importing accounts.");
+        }
+    }
+
+
+    @Override
+    public void exportAccounts() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("./output/output.txt"))) {
+            for (Map.Entry<Integer, T> entry : accounts.entrySet()) {
+                Account account = entry.getValue();
+                System.out.println("Exporting Account: " + account.getAccountId()); // Debug statement
+                writer.println(account.getAccountId() + "," +
+                        account.getAccountNumber() + "," +
+                        account.getAccountHolderName() + "," +
+                        account.getBalance() + "," +
+                        account.getAccountType());
+            }
+
+            System.out.println("Export completed successfully."); // Debug statement
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
